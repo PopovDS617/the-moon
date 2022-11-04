a58ef1927367eeed679f3f384987d573a7c38c03
PopovDS617 <popovds.bm@gmail.com>
2022-11-04 19:18:21 +0300

    finished

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import './App.css';
+import Moon from './components/moon-model/Moon';
+
+function App() {
+  return (
+    <>
+      <div className="layout">
+        <div className="canvas-container">
+          <Canvas id="three-canvas-container">
+            <Suspense fallback={<></>} />
+            <Moon />
+          </Canvas>
+        </div>
+      </div>
+
+      <div className="content">
+        <div className="article">
+          <div className="header">hello</div>
+          <p>Diameter: 3,475 km</p>
+          <p>Mass: 7.35 × 10^22 kg (0.01 Earths)</p>
+          <p>Distance from Earth: 385,000 km</p>
+          <p>Orbit period: 27.3 days</p>
+          <p>Surface temperature: -233 to 123 °C</p>
+          <p>Moonless: the Moon has no moons</p>
+          <p>Ringless: the Moon has no rings</p>
+          <p>Visitors: 12 people and more than 105 robotic spacecraft</p>
+          <p>Population: zero</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/moon-model/Moon.jsx
@@ -0,0 +1,52 @@
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  useTexture,
+} from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { useThree } from '@react-three/fiber';
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+
+const Moon = () => {
+  const { scene } = useThree();
+
+  const EnvironmentTexture = useLoader(TextureLoader, 'public/space2.jpg');
+  EnvironmentTexture.encoding = THREE.sRGBEncoding;
+  scene.background = EnvironmentTexture;
+
+  const moonRef = useRef(null);
+  useFrame(({}) => {
+    moonRef.current.rotation.y += 0.0007;
+  });
+  const moonTexture = useTexture('moon.jpg');
+
+  return (
+    <>
+      <PerspectiveCamera
+        makeDefault
+        position={[16, 0, -5]}
+        rotation={[0, 5, 0]}
+      />
+      <OrbitControls enabled={false} />
+      <mesh position={[0, 0, 9]} ref={moonRef} castShadow receiveShadow>
+        <sphereGeometry args={[6, 100, 100]} />
+        <meshStandardMaterial color="#ffffff" map={moonTexture} />
+      </mesh>
+
+      <pointLight
+        castShadow
+        args={['#ffffff', 5]}
+        position={[5, -5, -77]}
+        penumbra={1}
+        distance={100}
+        decay={0.6}
+      />
+    </>
+  );
+};
+
+export default Moon;
